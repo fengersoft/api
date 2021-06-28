a64589e5483abe4beaf4d1eff2332711dc73433f
qinxiaofeng <qinxiaofeng@hasng.cn>
2021-06-28 13:40:13 +0800

    修改编码检测

--- file/ioutils.cpp
+++ file/ioutils.cpp
@@ -7,17 +7,9 @@ QString readString(QByteArray data)
     QTextCodec* codec = QTextCodec::codecForName("UTF-8");
     QString text = codec->toUnicode(data.constData(), data.size(), &state);
     //等于是是GBK
-    if (state.invalidChars == 4)
+    if (state.invalidChars > 0)
     {
         text = QTextCodec::codecForName("GBK")->toUnicode(data);
-    }
-    else if (state.invalidChars == 3) //>>.3是unicode
-    {
-        text = QTextCodec::codecForName("Unicode")->toUnicode(data);
-    }
-    if (state.invalidChars >= 5)
-    {
-        text = QTextCodec::codecForName("GB2312")->toUnicode(data);
     }
     else
     {
